9a1b598f6bbf6c2162d6cb1d35ad4a37f2c7b439
haruya1102 <haruyaiioni@gmail.com>
2024-02-04 22:49:50 +0900

    setting main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './pages/LoginPage/LoginPage';
+import Dashboard from './pages/Dashboard/Dashboard';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebase/config';
 
-function App() {
+const App = () => {
+  const [user, loading, error] = useAuthState(auth);
+
+  if (loading) {
+    return <div>Loading...</div>; // 認証状態のロード中に表示するコンテンツ
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>; // エラーがあった場合に表示
+  }
+
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={user ? <Navigate to="/" /> : <LoginPage />} />
+        <Route path="/" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+        {/* 以下、必要に応じて他のルートを追加 */}
+        <Route path="*" element={<Navigate to={user ? "/" : "/login"} />} /> {/* 未定義のパスへのアクセスを処理 */}
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
